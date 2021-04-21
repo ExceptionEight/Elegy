9de2b6376a99711ddb48859e3158d53b330a525b
Dmitry <dememew@ya.ru>
2021-04-22 00:06:41 +0300

    minor improvements

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -72,6 +72,6 @@ String getContentType(String filename){
   if (filename.endsWith(".html")) return "text/html";
   else if (filename.endsWith(".css")) return "text/css";
   else if (filename.endsWith(".js")) return "application/javascript";
-  else if (filename.endsWith(".svg")) return "image/svg";
+  else if (filename.endsWith(".svg")) return "image/svg+xml";
   return "text/plain";
 }
