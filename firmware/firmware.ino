6fac366633776fa28ed624b52eee23e8d99333fe
Dmitry <dememew@ya.ru>
2021-12-28 02:33:36 +0300

    some improvements

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,17 +2,30 @@
 #include <ESP8266WebServer.h>
 #include <FS.h>
 #include <ESP8266FtpServer.h>
+#include <FastLED.h>
+#define NUM_LEDS 60
+#define PIN 5
 ESP8266WebServer server(80);
 FtpServer ftp;
 
+CRGB leds[NUM_LEDS];
+byte counter = 0;
 byte brightness = 100;
-byte accentColor = 15;
-byte saturation = 255;
-byte speed = 128;
+
+//unsigned char matrix [1][29][2];
+struct {
+  byte accentColor = 15;
+  byte speed = 128;
+  byte saturation = 255;
+} effect;
+
 
 void setup()
 {
+  pinMode (5, OUTPUT);
   pinMode (D4, OUTPUT);
+  FastLED.addLeds<WS2812, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(10);
   Serial.begin(115200);
   WiFi.begin("MGTS_GPON_E879", "77S33FGN");
   WiFi.softAP("Elegy", "12344321");
@@ -36,8 +49,11 @@ void setup()
   //  Serial.print(".");
   //}
   server.on("/powerSwitch", powerSwitch);
-  server.on("/config", config);
-  //server.on("/setWifiConnection", api);
+  server.on("/setBrightness", setBrightness);
+  server.on("/setAccentColor", setAccentColor);
+  server.on("/setSaturation", setSaturation);
+  server.on("/setSpeed", setSpeed);
+   server.on("/setSpeed", setMode);
   server.onNotFound([](){
     if (!handleFileRead(server.uri()))
     server.send (404, "text/plain", "DURILKA");
@@ -51,14 +67,41 @@ void setWifiConnection () {
 void powerSwitch() {
   if (server.arg("power")=="1") {
     digitalWrite(D4, LOW);
+    FastLED.setBrightness(0);
   }
   else {
     digitalWrite (D4, HIGH);
+    FastLED.setBrightness(brightness);
   }
   server.send(200);
 }
 
+void setBrightness () {
+  brightness = server.arg("value").toInt();
+  FastLED.setBrightness(brightness);
+  server.send(200);
+}
+
+void setAccentColor () {
+  effect.accentColor = server.arg("value").toInt();
+  server.send(200);
+}
+
+void setSaturation () {
+  effect.saturation = server.arg("value").toInt();
+}
+
+void setSpeed () {
+  effect.speed = server.arg("value").toInt();
+}
+
+void setMode () {
+  server.send(200);
+}
+
 void config() {
+  brightness = server.arg("brightness").toInt();
+  FastLED.setBrightness(server.arg("brightness").toInt());
   byte brightness = server.arg("brightness").toInt();
   byte accentColor = server.arg("accentColor").toInt();
   byte saturation = server.arg("saturation").toInt();
@@ -67,9 +110,18 @@ void config() {
   server.send(200);
 }
 
+void solidMode() {
+
+}
+
 void loop() {
   server.handleClient();
   ftp.handleFTP();
+
+  for (int i = 0; i < NUM_LEDS; i++){
+    leds[i] = CHSV(effect.accentColor, 255, 255);
+  }
+  FastLED.show();
 }
 
 bool handleFileRead(String path){
--- http/script.js
+++ http/script.js
@@ -1,20 +1,62 @@
-var power = false;
-var brightness = 100;
-var accentColor = 15;
-var saturation = 255;
-var speed = 128;
+let power = false;
+let brightness = 100;
 var synced = false;
+let buffer = {}
 
+let effects = {
+  'default': {
+
+  },
+  'solid': {
+    accentColor: 15,
+    saturation: 255
+  },
+  'pulse': {
+    accentColor: 15,
+    saturation: 255,
+    speed: 128
+  }
+}
 
 configUpdater();
+//let lastBrightnessSync = 0
+const setBrightness = value => {
+  buffer.brightness = value * 10
+}
+
+const setAccentColor = value => {
+  buffer.accentColor = value
+}
+
+function configUpdater () {
+  if (Object.keys(buffer).length > 0) {
+    if (buffer.mode) {
+      $.get(`setMode?value=${buffer.mode}`)
+      delete buffer.mode
+    }
+
+    if (buffer.brightness) {
+      $.get(`setBrightness?value=${buffer.brightness}`)
+      delete buffer.brightness
+    }
+
+    if (buffer.accentColor) {
+      $.get(`setAccentColor?value=${buffer.accentColor}`)
+      delete buffer.accentColor
+    }
+  }
+  setTimeout (configUpdater, 70);
+}
+/*
 function configUpdater() {
-  if (synced == false) {
+  if (!synced) {
     synced = true;
     brightness = document.getElementById("brightnessSlider").value;
     $.get("config?brightness=" + brightness*10 + "&accentColor=" + accentColor + "&saturation=" + saturation + "&speed=" + speed);
   }
   setTimeout (configUpdater, 250);
 }
+*/
 
 function powerSwitch(){
   if (power) {
