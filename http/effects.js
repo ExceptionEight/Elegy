7d34aa20c4b76575dc12854428702cd73d965b58
ExceptionEight <42202104+ExceptionEight@users.noreply.github.com>
2021-12-28 17:47:48 +0300

    added effects object

---
+++ http/effects.js
@@ -0,0 +1,22 @@
+const effects = {
+  'default': {
+    'name': 'Дефолтный'
+  },
+  'solid': {
+    name: 'Монотонный',
+    accentColor: 0,
+    saturation: 255
+  },
+  'pulse': {
+    name: 'Пульсация',
+    accentColor: 0,
+    saturation: 0,
+    speed: 128
+  },
+  'epilepsy': {
+    name: 'Привет эпилептики',
+    accentColor: 0,
+    saturation: 0,
+    speed: 128
+  },
+}
--- http/script.js
+++ http/script.js
@@ -1,25 +1,96 @@
 let power = false;
 let brightness = 100;
-var synced = false;
 let buffer = {}
+let selectedEffect = {name: 'solid', isSettingsActive: false}
 
-let effects = {
-  'default': {
+const getParams = () => {
 
-  },
-  'solid': {
-    accentColor: 15,
-    saturation: 255
-  },
-  'pulse': {
-    accentColor: 15,
-    saturation: 255,
-    speed: 128
+}
+
+const showEffects = () => {
+  const amount = Object.keys(effects).length
+  for (let i = 0; i < amount; i++) {
+    const commonEffectContainer = document.createElement('div')
+    commonEffectContainer.className = 'commonEffectContainer'
+    commonEffectContainer.id = Object.keys(effects)[i]
+    const effectName = document.createElement('div')
+    effectName.className = 'effectName'
+    effectName.innerHTML = effects[Object.keys(effects)[i]]['name']
+
+    effectName.onclick = () => {
+      //document.getElementById(selectedEffect).className =
+      if (selectedEffect.name == Object.keys(effects)[i] && !selectedEffect.isSettingsActive) {
+        showEffectSettings(commonEffectContainer, effects[Object.keys(effects)[i]])
+        selectedEffect.isSettingsActive = true
+      } else {
+        selectedEffect.name = Object.keys(effects)[i]
+        selectedEffect.isSettingsActive = false
+        document.getElementById('settingsEffectContainer').remove()
+      }
+    }
+
+    commonEffectContainer.appendChild(effectName)
+    document.getElementById('effectsContainer').appendChild(commonEffectContainer)
   }
+}
+showEffects()
+
+const showEffectSettings = (container, effect) => {
+  const settingsEffectContainer = document.createElement('div')
+  settingsEffectContainer.className = 'settingsEffectContainer'
+  settingsEffectContainer.id = 'settingsEffectContainer'
+  if (effect.accentColor != null) {
+    const sliderWrapper = document.createElement('div')
+    sliderWrapper.className = 'effectSliderWrapper'
+    const slider = document.createElement('input')
+    slider.className = 'hsvSlider'
+    slider.type = 'range'
+    slider.min = 1
+    slider.max = 255
+    slider.value = effect.accentColor
+    slider.oninput = () => {
+      setAccentColor(slider.value)
+    }
+    sliderWrapper.appendChild(slider)
+    settingsEffectContainer.appendChild(sliderWrapper)
+  }
+
+  if (effect.saturation != null) {
+    const sliderWrapper = document.createElement('div')
+    sliderWrapper.className = 'effectSliderWrapper'
+    const slider = document.createElement('input')
+    slider.className = 'saturationSlider'
+    slider.type = 'range'
+    slider.min = 1
+    slider.max = 255
+    slider.value = effect.saturation
+    slider.oninput = () => {
+      setSaturation(slider.value)
+    }
+    sliderWrapper.appendChild(slider)
+    settingsEffectContainer.appendChild(sliderWrapper)
+  }
+
+  if (effect.speed != null) {
+    const sliderWrapper = document.createElement('div')
+    sliderWrapper.className = 'effectSliderWrapper'
+    const slider = document.createElement('input')
+    slider.className = 'speedSlider'
+    slider.type = 'range'
+    slider.min = 1
+    slider.max = 255
+    slider.value = effect.speed
+    slider.oninput = () => {
+      setSpeed(slider.value)
+    }
+    sliderWrapper.appendChild(slider)
+    settingsEffectContainer.appendChild(sliderWrapper)
+  }
+  container.appendChild(settingsEffectContainer)
+console.log (effect)
 }
 
 configUpdater();
-//let lastBrightnessSync = 0
 const setBrightness = value => {
   buffer.brightness = value * 10
 }
@@ -28,6 +99,13 @@ const setAccentColor = value => {
   buffer.accentColor = value
 }
 
+const setSaturation = value => {
+  buffer.saturation = value
+}
+
+const setSpeed = value => {
+  buffer.speed = value
+}
 function configUpdater () {
   if (Object.keys(buffer).length > 0) {
     if (buffer.mode) {
@@ -47,16 +125,6 @@ function configUpdater () {
   }
   setTimeout (configUpdater, 70);
 }
-/*
-function configUpdater() {
-  if (!synced) {
-    synced = true;
-    brightness = document.getElementById("brightnessSlider").value;
-    $.get("config?brightness=" + brightness*10 + "&accentColor=" + accentColor + "&saturation=" + saturation + "&speed=" + speed);
-  }
-  setTimeout (configUpdater, 250);
-}
-*/
 
 function powerSwitch(){
   if (power) {
